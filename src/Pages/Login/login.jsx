be50952ea70b3cb817acb2edcc56005e871a7c3e
Banjihopes <banjihopes@gmail.com>
2023-02-13 21:47:00 +0100

    third commit

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -2,16 +2,33 @@ import { Link } from "react-router-dom"
 import "./login.css"
 
 export default function Login() {
+
+    const userRef = userRef()
+    const passwordRef = userRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input type="text" className="loginInput" placeholder="Enter your email..." />
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input
+                    type="text"
+                    className="loginInput"
+                    placeholder="Enter your username..."
+                    ref={userRef}
+                />
                 <label>Password</label>
-                <input type="password" className="loginInput" placeholder="Enter your password..." />
+                <input
+                    type="password"
+                    className="loginInput"
+                    placeholder="Enter your password..."
+                    ref={userRef}
+                />
                 <button className="loginButton">
-                    <Link className="link" to="/login">Login</Link>
+                    <Link className="link" to="/login" type="submit">Login</Link>
                 </button>
             </form>
             <button className="loginRegButton">
