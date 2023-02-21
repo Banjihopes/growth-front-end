3e54eb0c1fa21d60b300b866f356dc384769f90d
Banjihopes <banjihopes@gmail.com>
2023-02-21 08:17:55 +0100

    8th commit

--- src/Components/SinglePost/singlepost.jsx
+++ src/Components/SinglePost/singlepost.jsx
@@ -9,7 +9,7 @@ export default function SinglePost() {
     const location = useLocation()
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({})
-    const PF = "http://localhost:3000/images/"
+    const PF = "http://localhost:3000/api/images/"
     const { user } = useContext(Context);
     const [title, setTitle] = useState("")
     const [desc, setDesc] = useState("")
--- src/Pages/Write/write.jsx
+++ src/Pages/Write/write.jsx
@@ -30,7 +30,7 @@ export default function Write() {
         }
         try {
             const res = await axios.post("/posts", newPost)
-            window.location.replace("/post" + res.data._id)
+            window.location.replace("/post/" + res.data._id)
         } catch (err) { }
     }
     return (
