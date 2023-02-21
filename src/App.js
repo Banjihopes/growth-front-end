6522ace614490455494cbc1b99c718db4f8cda87
Banjihopes <banjihopes@gmail.com>
2023-02-21 17:56:31 +0100

    9th commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Write from "./Pages/Write/write";
 import Settings from "./Pages/Settings/settings";
 import Login from "./Pages/Login/login";
 import Register from "./Pages/Register/register";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import { Context } from "./context/Context";
-
 
 function App() {
   const { user } = useContext(Context);
+
+  useEffect(() => {
+    // This fetch code will execute after the component mounts
+    fetch(process.env.REACT_APP_API_URL + '/api/')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -29,7 +33,6 @@ function App() {
           <Route path='/post/:postid' element={<Single />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
